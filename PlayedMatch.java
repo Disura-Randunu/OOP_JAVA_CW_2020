9bb035b9acb1679b6858dffe9c7510cbd9b1a0ab
DisuraRandunu <disurarandunu@gmail.com>
2020-12-02 13:30:44 +0530

    Major Changes

---
+++ PlayedMatch.java
@@ -0,0 +1,96 @@
+import java.time.LocalDate;
+
+public class PlayedMatch {
+
+    private FootballClub homeClub;     // Based on which location where the match takes place
+    private FootballClub visitorClub;     // Based on which location where the match takes place
+    private LocalDate matchDate;
+    private String matchLocation;
+
+    private int homeScoredGoals;
+    private int visitorScoredGoals;
+
+    public PlayedMatch(FootballClub homeClub, FootballClub visitorClub, LocalDate matchDate) {
+        this.homeClub = homeClub;
+        this.visitorClub = visitorClub;
+        this.matchDate = matchDate;
+        this.matchLocation = homeClub.getLocation();
+    }
+
+    public String getMatchLocation() {
+        return matchLocation;
+    }
+
+    public FootballClub getHomeClub() {
+        return homeClub;
+    }
+
+    public void setHomeClub(FootballClub homeClub) {
+        this.homeClub = homeClub;
+    }
+
+    public FootballClub getVisitorClub() {
+        return visitorClub;
+    }
+
+    public void setVisitorClub(FootballClub visitorClub) {
+        this.visitorClub = visitorClub;
+    }
+
+    public LocalDate getMatchDate() {
+        return matchDate;
+    }
+
+    public void setMatchDate(LocalDate matchDate) {
+        this.matchDate = matchDate;
+    }
+
+    public int getHomeScoredGoals() {
+        return homeScoredGoals;
+    }
+
+    public void setHomeScoredGoals(int homeClubGoalsScored) {
+        this.homeScoredGoals = homeClubGoalsScored;
+    }
+
+    public int getVisitorScoredGoals() {
+        return visitorScoredGoals;
+    }
+
+    public void setVisitorScoredGoals(int visitorClubGoalsScored) {
+        this.visitorScoredGoals = visitorClubGoalsScored;
+    }
+
+    public void calculateStats(){
+
+        // Set Played Matches
+        homeClub.setPlayedMatches(homeClub.getPlayedMatches() + 1);
+        visitorClub.setPlayedMatches(visitorClub.getPlayedMatches() + 1);
+
+        // Set Scored Goals
+        homeClub.setScoredGoals(homeClub.getScoredGoals() + homeScoredGoals);
+        visitorClub.setScoredGoals(visitorClub.getScoredGoals() + visitorScoredGoals);
+
+        // Set Received Goals
+        homeClub.setReceivedGoals(homeClub.getReceivedGoals() + visitorScoredGoals);
+        visitorClub.setReceivedGoals(visitorClub.getReceivedGoals() + homeScoredGoals);
+
+        // Set Wins/Defeats/Draws/Points
+        if (homeScoredGoals > visitorScoredGoals){
+            homeClub.setWins(homeClub.getWins() + 1);
+            homeClub.setPoints(homeClub.getPoints() + 3);
+            visitorClub.setDefeats(visitorClub.getDefeats() + 1);
+
+        }else if (visitorScoredGoals > homeScoredGoals){
+            visitorClub.setWins(visitorClub.getWins() + 1);
+            visitorClub.setPoints(visitorClub.getPoints() + 3);
+            homeClub.setDefeats(homeClub.getDefeats() + 1);
+
+        } else if (visitorScoredGoals == homeScoredGoals){
+            homeClub.setDraws(homeClub.getDraws() + 1);
+            homeClub.setPoints(homeClub.getPoints() + 1);
+            visitorClub.setDraws(visitorClub.getDraws() + 1);
+            visitorClub.setPoints(visitorClub.getPoints() + 1);
+        }
+    }
+}
